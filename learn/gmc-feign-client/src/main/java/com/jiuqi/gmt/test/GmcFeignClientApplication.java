cc5a292d6b3722483ab639899f9468d87182e2c7
lipengfei <lipengfei@jiuqi.com.cn>
2019-08-07 21:37:53 +0800

    test
    Signed-off-by: lipengfei <lipengfei@jiuqi.com.cn>

--- learn/gmc-feign-client/src/main/java/com/jiuqi/gmt/test/GmcFeignClientApplication.java
+++ learn/gmc-feign-client/src/main/java/com/jiuqi/gmt/test/GmcFeignClientApplication.java
@@ -2,8 +2,10 @@ package com.jiuqi.gmt.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GmcFeignClientApplication {
 
 	public static void main(String[] args) {
