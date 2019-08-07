4d91a73ed0ffee93f391fee737edc8592f60b185
lipengfei <lipengfei@jiuqi.com.cn>
2019-08-07 20:25:27 +0800

    test
    Signed-off-by: lipengfei <lipengfei@jiuqi.com.cn>

--- learn/gmc-test-01/src/main/java/com/jiuqi/gmt/test/GmcTest01Application.java
+++ learn/gmc-test-01/src/main/java/com/jiuqi/gmt/test/GmcTest01Application.java
@@ -2,8 +2,10 @@ package com.jiuqi.gmt.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GmcTest01Application {
 
 	public static void main(String[] args) {
